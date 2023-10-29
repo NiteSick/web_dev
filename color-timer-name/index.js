4f12bf42abc9501688afabe66f2289b9343c4e8f
Deepanshu Jangid <deepanshu21270@gmail.com>
2023-10-29 20:33:19 +0530

    bg color changge div created

---
+++ color-timer-name/index.js
@@ -0,0 +1,17 @@
+function generateRandomColor() {
+    // Generating random values for RGB
+    var red = Math.floor(Math.random() * 200);
+    var green = Math.floor(Math.random() * 210);
+    var blue = Math.floor(Math.random() * 230);
+  
+    // Creating the RGB string
+    var randomColor = "rgb(" + red + "," + green + "," + blue + ")";
+  
+    return randomColor;
+  }
+
+function changeBackgroundColor(){
+    let bg = document.getElementById("color-section");
+    bg.style.backgroundColor = generateRandomColor();
+}
+  
