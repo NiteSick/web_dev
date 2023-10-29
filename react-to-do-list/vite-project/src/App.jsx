0c19312f25a571bcda82e5654357216d2b6b8c80
Deepanshu Jangid <deepanshu21270@gmail.com>
2023-10-29 17:52:14 +0530

    tech document started

--- react-to-do-list/vite-project/src/App.jsx
+++ react-to-do-list/vite-project/src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
 
-import './App.css'
+
 
 function App() {
-  
-  return (
+  const[newItem,setNewItem] =  useState("")
+  const[todos, setNewTodos] = useState("")
+  function handleSubmit(e){
+      e.preventDefault();
+  }
+  return(
     <>
-     This is my first react app using vite
+    <form onSubmit={handleSubmit} className='new-item-form'>
+      <div className='form-row'>
+        <label htmlFor='item'>New item</label>
+        <input value={newItem}
+         type="text" 
+         id='item'
+         onChange={e=>setNewItem(e.target.value)}/>
+      </div>
+      <button className='btn'>
+        Add
+      </button>
+    </form>
+    <h1 className='header'>Todo List</h1>
+    <ul className='list'>
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" />Item1
+        </label>
+        <button className='btn btn-danger'>Delete</button>
+      </li>
+    </ul>
     </>
   )
 }
